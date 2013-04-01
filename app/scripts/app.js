e8147c97744561a0cd2830e29d4aaab75f8e8677
Nick Heiner <nick.heiner@opower.com>
2013-04-01 19:41:36 -0400

    yo angular import

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('directiveGuideApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
