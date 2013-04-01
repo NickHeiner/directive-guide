ca63b6ee9d2b2059f422eeb9832b05ec2aef082b
Nick Heiner <nick.heiner@opower.com>
2013-04-01 19:59:16 -0400

    adding angular integration to default template

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('directiveGuideApp', [])
+    angular.module('directiveGuideApp', [])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,30 @@
 'use strict';
 
 angular.module('directiveGuideApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function($scope) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+
+        $scope.sampleDirective = {
+            priority: 0,
+            template: '<div></div>',
+            templateUrl: 'directive.html',
+            replace: false,
+            transclude: false,
+            restrict: 'A',
+            scope: false,
+            compile: function compile(tElement, tAttrs, transclude) {
+                return {
+                    pre: function preLink(scope, iElement, iAttrs, controller) {
+                    },
+                    post: function postLink(scope, iElement, iAttrs, controller) {
+                    }
+                }
+            },
+            link: function postLink(scope, iElement, iAttrs) {
+            }
+        };
+    });
